111c7703bbee6063a6d4f586f13bb5403109dbd9
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-02-08 10:32:07 -0800

    added weather app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form id="search-form" className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  className="form-control"
+                  id="city-input"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1 id="city">city</h1>
+            <ul>
+              <li>
+                Last updated: <span id="date"></span>
+              </li>
+              <li id="description"></li>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix weather-temperature">
+                <img src="" alt="Clear" id="icon" className="float-left" />
+                <div className="float-left">
+                  <strong id="temperature"></strong>
+                  <span className="units"> °C</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity"></span>%
+                </li>
+                <li>
+                  Wind: <span id="wind"></span> m/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+      </div>
     </div>
   );
 }
