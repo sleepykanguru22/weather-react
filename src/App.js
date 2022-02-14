4008eb121bab4fb21c4b9a20ca5d8f5e63d1ede5
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-02-14 14:18:25 -0800

    Added weather search engine

--- src/App.js
+++ src/App.js
@@ -1,65 +1,13 @@
 import React from "react";
 import "./styles.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <form id="search-form" className="mb-3">
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    placeholder="Type a city.."
-                    className="form-control"
-                    id="city-input"
-                    autocomplete="off"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary w-100"
-                  />
-                </div>
-              </div>
-            </form>
-            <div className="overview">
-              <h1 id="city">city</h1>
-              <ul>
-                <li>
-                  Last updated: <span id="date"></span>
-                </li>
-                <li id="description"></li>
-              </ul>
-            </div>
-            <div className="row">
-              <div className="col-6">
-                <div className="clearfix weather-temperature">
-                  <img src="" alt="Clear" id="icon" className="float-left" />
-                  <div className="float-left">
-                    <strong id="temperature"></strong>
-                    <span className="units"> °C</span>
-                  </div>
-                </div>
-              </div>
-              <div className="col-6">
-                <ul>
-                  <li>
-                    Humidity: <span id="humidity"></span>%
-                  </li>
-                  <li>
-                    Wind: <span id="wind"></span> m/h
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="weather-forecast" id="forecast"></div>
-          </div>
-        </div>
+        <Weather defaultCity="San Francisco" />
+
         <footer>
           <a href="https://github.com/sleepykanguru22/weather-react">
             Open source project
--- src/Weather.js
+++ src/Weather.js
@@ -1,65 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      hello from weather
-      <div className="weather-app-wrapper">
-        <div className="weather-app">
-          <form id="search-form" className="mb-3">
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Type a city.."
-                  className="form-control"
-                  id="city-input"
-                  autocomplete="off"
-                />
-              </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="btn btn-primary w-100"
-                />
-              </div>
-            </div>
-          </form>
-          <div className="overview">
-            <h1 id="city">city</h1>
-            <ul>
-              <li>
-                Last updated: <span id="date"></span>
-              </li>
-              <li id="description"></li>
-            </ul>
-          </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-6">
-              <div className="clearfix weather-temperature">
-                <img src="" alt="Clear" id="icon" className="float-left" />
-                <div className="float-left">
-                  <strong id="temperature"></strong>
-                  <span className="units"> °C</span>
-                </div>
-              </div>
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
             </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  Humidity: <span id="humidity"></span>%
-                </li>
-                <li>
-                  Wind: <span id="wind"></span> m/h
-                </li>
-              </ul>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
